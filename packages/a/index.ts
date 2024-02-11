441eeb08172322f5448c98df658c83bf9b1389f5
Horai <44516902+pensuke81@users.noreply.github.com>
2024-02-12 06:33:56 +0900

    chore: improve comments

--- packages/a/index.ts
+++ packages/a/index.ts
@@ -1,9 +1,6 @@
 /**
  * sum two numbers
- * @param a - first number
- * @param b - second number
- * @returns sum of a and b
  */
-export const a = (a: number, b: number) => {
+export const a = (a: number, b: number): number => {
   return a + b
 }
